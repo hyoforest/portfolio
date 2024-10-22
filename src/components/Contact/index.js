47706aff7398f212f604aa691f7636df4fff9ecb
hyorim <hyoforest813@gmail.com>
2024-10-22 11:24:22 +0900

    1022

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -7,6 +7,9 @@ function Contact(){
         <h2>contact</h2>
         <div className="">
           <h3>연락처</h3>
+          <p>010.5967.9521</p>
+          <p>hyoforest813@gmail.com</p>
+          <p>깃허브 주소</p>
         </div>
         <Footer/>
       </section>
